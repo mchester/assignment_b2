86ebe8e152055352a6e7921409b45a1ab3925d8a
mchester <marshall.chester@alumni.ubc.ca>
2021-11-04 11:53:53 -0700

    Initial commit

---
+++ R/quick_boxplot.R
@@ -0,0 +1,19 @@
+#' boxplot quick function
+#' this function creates a quick boxplot for visualizing
+#' categorical and continuous variables
+#' @params {{x}} is a categorical variable from column in dataset (diagonosis)
+#' @params {{y}} is a continuous variable from column in dataset (select any continuous column)
+#' @return a tidy, simple boxplot
+#' @details the function will keep NA values
+#' @details the function will be tested for the creation and printing of the object, as well as correct axis labels
+#' @examples
+#' quick_boxplot(cancer_sample, diagnosis, perimeter_mean)
+#' quick_boxplot(cancer_sample, diagnosis, symmetry_mean)
+#' quick_boxplot(cancer_sample, diagnosis, area_mean)
+#' @export
+
+quick_boxplot <- function(data, x, y, na.rm = FALSE) {
+  (ggplot(data, aes({{x}}, {{y}}))) +
+    geom_boxplot()
+}
+
