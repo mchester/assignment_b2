2f14925bd4aaff6e103151f95381a8a14f26a2a5
mchester <marshall.chester@alumni.ubc.ca>
2021-11-04 17:04:30 -0700

    changes

--- R/quick_boxplot.R
+++ R/quick_boxplot.R
@@ -7,13 +7,13 @@
 #' @details the function will keep NA values
 #' @details the function will be tested for the creation and printing of the object, as well as correct axis labels
 #' @examples
-#' quick_boxplot(cancer_sample, diagnosis, perimeter_mean)
-#' quick_boxplot(cancer_sample, diagnosis, symmetry_mean)
-#' quick_boxplot(cancer_sample, diagnosis, area_mean)
+#' quick_boxplot(datateachr::cancer_sample, diagnosis, perimeter_mean)
+#' quick_boxplot(datateachr::cancer_sample, diagnosis, symmetry_mean)
+#' quick_boxplot(datateachr::cancer_sample, diagnosis, area_mean)
 #' @export
 
 quick_boxplot <- function(data, x, y, na.rm = FALSE) {
-  (ggplot(data, aes({{x}}, {{y}}))) +
-    geom_boxplot()
+  (ggplot2::ggplot(data, aes({{x}}, {{y}}))) +
+    ggplot2::geom_boxplot()
 }
 
